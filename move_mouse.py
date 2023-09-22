2c80bb4be1d1ed58caaea11f3f4bd9a6c3097007
Eugenio Cataldo <eugenio.cataldo@gmail.com>
2023-09-22 15:07:48 +0200

    Put everything into functions
    Split the actions to be done into functions since some were repeated

--- move_mouse.py
+++ move_mouse.py
@@ -57,40 +57,53 @@ STATUS_TAB_IN_RECORD = (1671, 713)
 ADD_USER = (905, 602)
 duration = 1
 
-
-text_to_write = []
-
-#pyautogui.click(pyautogui.position())
-
-#pyautogui.scroll(-1000) #TODO: Check if the plus position changes
-
-pyautogui.moveTo(ADD_USER, duration = duration)
-
-pyautogui.moveTo(SUBMIT_SEARCH, duration = duration)
-
-"""
-pyautogui.click(PROCESS_TAB)
-pyautogui.click(SEARCH_PROCESS)
-pyautogui.click(SEARCH_TEXTBOX)
-pyautogui.typewrite(text_to_write)
-pyautogui.click(SUBMIT_SEARCH)
-pyautogui.click(VIEW_INFOCARD)
-pyautogui.click(STATUS_TAB_IN_RECORD)
-
-go_to_image_and_click(modify_step_icon) #TODO: Add modify step icon
-
-pyautogui.typewrite(user_to_add)
-pyautogui.press('enter')
-pyautogui.scroll(X) #TODO: Find out how much I need to scroll or adapt this to big screen so don't need it
-go_to_image_and_click(add_icon) #TODO: Add image of the add icon 
-pyautogui.click(save_button) #TODO: Find coordinate of the button or image
-pyautogui.typewrite("Adding user to record")
-pyautogui.click(SAVE_BUTTON) #TODO: Add coordinates for this because there are two "save" in the image
+print("Mouse is now at")
+print(pyautogui.position())
 
 
+def search_record(text_to_write):
+
+    """
+    This function runs a search function within processes in MasterControl. You need to be in the main page (you need to see the tabs on the left)
+    """
+
+    go_to_image_and_click(process_tab)
+    go_to_image_and_click(search_button)
+    pyautogui.typewrite(text_to_write)
+    go_to_image_and_click(submit_search_button)
 
 
+def add_user_to_record():
 
-#pyautogui.moveTo(PROCESS_TAB)
+    """
+    This function adds a user to a record. You need to be inside a search and only have one record returned in your search
+    """
 
-"""
+    go_to_image_and_click(view_infocard_button)
+    go_to_image_and_click(status_tab)
+    go_to_image_and_click(modify_step_icon)
+    go_to_image_and_click(add_user_icon)
+
+    # Write the user to add in the textbox and press enter
+    pyautogui.typewrite(user_to_add) 
+    pyautogui.press('enter')
+    go_to_image_and_click(add_button)
+    go_to_image_and_click(save_button)
+    pyautogui.typewrite("Adding user to record")
+    go_to_image_and_click(save_button_second_step) #TODO: Test if it finds the right one 
+    go_to_image_and_click(close)
+
+
+def move_record_forward():
+
+    """
+    This function moves a record to the next step (approves it). You need to be inside a search and only have one record returned in your search
+    """
+
+    go_to_image_and_click(open_task_button)
+    go_to_image_and_click(sign_off_button)
+    go_to_image_and_click(empty_space_below_status) #TODO: Test whether you can see this, since you'd need to click on empty space
+    go_to_image_and_click(data_approval)
+    go_to_image_and_click(empty_space_below_comments) #TODO: Test whether you can see this, since you'd need to click on empty space
+    go_to_image_and_click(sign_off_button) #TODO: Check whether I need to add a comment or if it can be e
+
